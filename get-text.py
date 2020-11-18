904cafe749f47f9d5b4de2c56b8bf65885b871fa
sunny-aguilar <sunny.aguilar@gmail.com>
2020-11-18 13:21:17 -0800

    text scraper

---
+++ get-text.py
@@ -0,0 +1,11 @@
+import bs4
+import urllib.request
+
+link = "https://thefraternityadvisor.com/300-situations-sorority-and-fraternity-presidents-must-overcome/"
+webpage=str(urllib.request.urlopen(link).read())
+soup = bs4.BeautifulSoup(webpage)
+
+print(soup.get_text())
+
+# create a file
+
